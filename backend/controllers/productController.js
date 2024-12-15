7583d92ab8aa1a3caeb3784abc0570eac2e4b3fe
Tae <phenphak.forlearn@gmail.com>
2024-12-15 16:22:45 +0700

    fix(product): set user ID in request body for product creation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,6 +25,7 @@ export const getProduct = catchAsyncErrors(async (req, res) => {
 
 //create new product  => /api/admin/products
 export const newProduct = catchAsyncErrors(async (req, res) => {
+  req.body.user = req.user._id;
   const product = await Product.create(req.body);
   res.status(200).json({
     product,
