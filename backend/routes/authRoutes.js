7b7f7ecdfcb03f966fc15e1dad36de171d1e5f2d
Tae <phenphak.forlearn@gmail.com>
2024-12-16 17:34:31 +0700

    feat: Add route for getting user profile with authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,18 +1,24 @@
 import express from "express";
 import {
   forgotPassword,
+  getUserProfile,
   loginUser,
   logout,
   registerUser,
   resetPassword,
 } from "../controllers/authController.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
+
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser,getUserProfile);
+
 
 export default router;
