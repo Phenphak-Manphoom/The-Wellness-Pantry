a71b540baac1b62645176c6ff75741ff15a6fb29
Tae <phenphak.forlearn@gmail.com>
2024-12-26 18:40:26 +0700

    feat(routes): add review-related routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import {
   deleteProduct,
+  deleteReview,
   getProduct,
   getProductDetails,
+  getProductReviews,
   newProduct,
   updateProduct,
 } from "../controllers/productController.js";
 import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+import { createProductReview } from "../controllers/productController.js";
 
 const router = express.Router();
 
@@ -23,5 +26,14 @@ router
 router
   .route("/admin/products/:id")
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router
+  .route("/reviews")
+  .get(isAuthenticatedUser, getProductReviews)
+  .put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/admin/reviews")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
 
 export default router;
