3c889871b8aafb8418609493428094f9940f93d0
Tae <phenphak.forlearn@gmail.com>
2025-01-30 16:19:17 +0700

    feat: assign admin user ID to seeded products

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -1,13 +1,27 @@
 import mongoose from "mongoose";
 import Product from "../models/product.js";
 import products from "./data.js";
+import User from "../models/user.js";
 
 const seedProducts = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/The-Wellness-Pantry");
     await Product.deleteMany();
     console.log("Products are deleted");
-    await Product.insertMany(products);
+
+    // ดึง user จากฐานข้อมูล (สมมติว่าใช้ user คนแรกในฐานข้อมูล)
+    const adminUser = await User.findOne(); // ตรวจสอบว่ามี user หรือไม่
+    if (!adminUser) {
+      console.log("No user found in the database!");
+      process.exit(1);
+    }
+    // เติมค่า user ให้กับสินค้าแต่ละตัว
+    const productsWithUser = products.map((product) => ({
+      ...product,
+      user: adminUser._id, // เพิ่ม user._id ในสินค้าแต่ละตัว
+    }));
+
+    await Product.insertMany(productsWithUser);
     console.log("Products are added");
 
     process.exit();
