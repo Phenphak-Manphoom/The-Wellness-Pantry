a9323d9f816c46415755bece75ec298363d74e9a
Tae <phenphak.forlearn@gmail.com>
2025-01-30 16:10:19 +0700

    feat: add success flag to newProduct response

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,6 +28,7 @@ export const newProduct = catchAsyncErrors(async (req, res) => {
   req.body.user = req.user._id;
   const product = await Product.create(req.body);
   res.status(200).json({
+    success: true,
     product,
   });
 });
