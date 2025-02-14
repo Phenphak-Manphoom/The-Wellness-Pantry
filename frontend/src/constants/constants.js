ee875f56ccc144ef074e1baebc8405c70b6830f0
Tae <phenphak.forlearn@gmail.com>
2025-02-14 15:15:45 +0700

    feat(categories): add predefined product categories for filtering

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,9 @@
+export const PRODUCT_CATEGORIES = [
+    "Meals",
+    "Snacks & Desserts",
+    "Drinks",
+    "Breakfast",
+    "Kids Healthy Food",
+    "Healthy Oils & Spreads",
+    "Organic",
+  ];
