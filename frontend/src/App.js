87a20646e5c266a3659778c1e4afc5ac5ac4d08e
Tae <phenphak.forlearn@gmail.com>
2025-03-06 09:54:13 +0700

    feat: Add protected routes for profile management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,11 @@ import { Toaster } from "react-hot-toast";
 import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import ProtectRoute from "./components/auth/ProtectRoute";
+import Profile from "./components/user/Profile";
+import UpdateProfile from "./components/user/UpdateProfile";
+import UploadAvatar from "./components/user/UploadAvatar";
+import UpdatePassword from "./components/user/UpdatePassword";
 
 function App() {
   return (
@@ -19,6 +24,42 @@ function App() {
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
+          <Route
+            path="/me/profile"
+            element={
+              <ProtectRoute>
+                <Profile />
+              </ProtectRoute>
+            }
+          />
+
+          <Route
+            path="/me/update_profile"
+            element={
+              <ProtectRoute>
+                <UpdateProfile />
+              </ProtectRoute>
+            }
+          />
+
+          <Route
+            path="/me/upload_avatar"
+            element={
+              <ProtectRoute>
+                <UploadAvatar />
+              </ProtectRoute>
+            }
+          />
+
+          <Route
+            path="/me/update_password"
+            element={
+              <ProtectRoute>
+                <UpdatePassword />
+              </ProtectRoute>
+            }
+          />
         </Routes>
 
         <Footer />
