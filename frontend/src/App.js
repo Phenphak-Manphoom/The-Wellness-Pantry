bbfe04ad04e35fc9b77039596bdf6ed6868f8598
Tae <phenphak.forlearn@gmail.com>
2025-03-11 12:50:00 +0700

    feat: add route for cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import UploadAvatar from "./components/user/UploadAvatar";
 import UpdatePassword from "./components/user/UpdatePassword";
 import ForgotPassword from "./components/auth/ForgotPassword";
 import ResetPassword from "./components/auth/ResetPassword";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
@@ -65,6 +66,8 @@ function App() {
               </ProtectRoute>
             }
           />
+
+          <Route path="/cart" element={<Cart />} />
         </Routes>
 
         <Footer />
