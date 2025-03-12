c3d9cacc5119210a5ba81e1133dfd2d24ef1787b
Tae <phenphak.forlearn@gmail.com>
2025-03-12 11:23:37 +0700

    style(cart): improve cart page UI with better layout and styles

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MetaData from "../layout/MetaData";
 import { useDispatch, useSelector } from "react-redux";
-import { RiDeleteBin6Fill } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { setCartItem, removeCartItem } from "../../redux/features/cartSlice";
 
 const Cart = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { cartItems } = useSelector((state) => state.cart);
 
   const increaseQty = (item, quantity) => {
@@ -38,114 +39,235 @@ const Cart = () => {
     dispatch(removeCartItem(id));
   };
 
+  const checkoutHandler = () => {
+    navigate("/shipping");
+  };
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <MetaData title={"Your Cart"} />
-
-      {/* ทำให้ส่วนนี้ขยายเต็มพื้นที่ที่เหลือ */}
-      <div className="flex-grow">
-        {cartItems?.length === 0 ? (
-          <h2 className="mt-5 text-center text-xl font-semibold">
-            Your Cart is Empty
-          </h2>
-        ) : (
-          <>
-            <h2 className="mt-5 text-2xl font-bold text-gray-800">
-              Your Cart: <b>{cartItems?.length} items</b>
+      <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16 min-h-screen">
+        <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+          {cartItems?.length === 0 ? (
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+              Your Cart is Empty
             </h2>
+          ) : (
+            <>
+              <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+                Your Cart: <b>{cartItems?.length} Items</b>
+              </h2>
 
-            <div className="flex flex-col lg:flex-row justify-between">
-              <div className="w-full lg:w-2/3">
-                {cartItems?.map((item) => (
-                  <div key={item.product}>
-                    <hr className="my-4 border-gray-300" />
-                    <div className="flex items-center justify-between p-4 bg-white shadow-md rounded-md">
-                      <div className="w-24 h-24">
-                        <img
-                          src={item?.image}
-                          alt={item?.name}
-                          className="w-full h-full object-cover rounded-md"
-                        />
+              <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+                <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl space-y-3">
+                  {cartItems?.map((item) => (
+                    <>
+                      <div className="space-y-6">
+                        <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
+                          <div className="space-y-4 text-left md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                            <img
+                              className="h-20 w-20 dark:hidden "
+                              src={item?.image}
+                              alt={item?.name}
+                            />
+
+                            <label for="counter-input" className="sr-only">
+                              Choose quantity:
+                            </label>
+                            <div className="flex items-center justify-between md:order-3 md:justify-end">
+                              <div className="flex items-center">
+                                <button
+                                  type="button"
+                                  id="decrement-button"
+                                  data-input-counter-decrement="counter-input"
+                                  className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                                  onClick={() =>
+                                    decreaseQty(item, item.quantity)
+                                  }
+                                >
+                                  <svg
+                                    className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 18 2"
+                                  >
+                                    <path
+                                      stroke="currentColor"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      stroke-width="2"
+                                      d="M1 1h16"
+                                    />
+                                  </svg>
+                                </button>
+                                <input
+                                  type="text"
+                                  id="counter-input"
+                                  data-input-counter
+                                  className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
+                                  placeholder=""
+                                  value={item?.quantity}
+                                  required
+                                />
+                                <button
+                                  type="button"
+                                  id="increment-button"
+                                  data-input-counter-increment="counter-input"
+                                  className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                                  onClick={() =>
+                                    increaseQty(item, item.quantity)
+                                  }
+                                >
+                                  <svg
+                                    className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 18 18"
+                                  >
+                                    <path
+                                      stroke="currentColor"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      stroke-width="2"
+                                      d="M9 1v16M1 9h16"
+                                    />
+                                  </svg>
+                                </button>
+                              </div>
+                              <div className="text-end md:order-4 md:w-32">
+                                <p className="text-base font-bold text-gray-900 dark:text-white">
+                                  ฿{item?.price}
+                                </p>
+                              </div>
+                            </div>
+
+                            <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                              <Link
+                                to={`/products/{item?.product} THB`}
+                                className="text-base font-medium text-gray-900 hover:underline dark:text-white"
+                              >
+                                {item?.name}{" "}
+                              </Link>
+
+                              <div className="flex items-center gap-4">
+                                <button
+                                  type="button"
+                                  className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
+                                  onClick={() =>
+                                    removeCartItemHandler(item?.product)
+                                  }
+                                >
+                                  <svg
+                                    className="me-1.5 h-5 w-5"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                  >
+                                    <path
+                                      stroke="currentColor"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      stroke-width="2"
+                                      d="M6 18 17.94 6M18 18 6.06 6"
+                                    />
+                                  </svg>
+                                  Remove
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  ))}
+                </div>
+
+                <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
+                  <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+                    <p className="text-xl font-semibold text-gray-900 dark:text-white">
+                      Order summary
+                    </p>
+
+                    <div className="space-y-4">
+                      <div className="space-y-2">
+                        <dl className="flex items-center justify-between gap-4">
+                          <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                            Units
+                          </dt>
+                          <dd className="text-base font-medium text-gray-900 dark:text-white">
+                            {cartItems?.reduce(
+                              (acc, item) => acc + item?.quantity,
+                              0
+                            )}{" "}
+                            (Units)
+                          </dd>
+                        </dl>
                       </div>
 
-                      <div className="flex-1 px-4">
-                        <Link
-                          to={`/products/${item?.product}`}
-                          className="text-lg font-medium text-blue-600 hover:underline"
-                        >
-                          {item?.name}
-                        </Link>
-                      </div>
+                      <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
+                        <dt className="text-base font-bold text-gray-900 dark:text-white">
+                          Total
+                        </dt>
+                        <dd className="text-base font-bold text-gray-900 dark:text-white">
+                          ฿
+                          {cartItems
+                            ?.reduce(
+                              (acc, item) => acc + item?.quantity * item.price,
+                              0
+                            )
+                            .toFixed(2)}
+                        </dd>
+                      </dl>
+                    </div>
 
-                      <div className="text-lg font-semibold text-gray-700">
-                        ${item?.price}
-                      </div>
+                    <button
+                      type="button"
+                      className="flex w-full items-center justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                      onClick={checkoutHandler}
+                    >
+                      Proceed to Checkout
+                    </button>
 
-                      <div className="flex items-center">
-                        <button
-                          className="px-3 py-1 bg-red-500 text-white rounded-md"
-                          onClick={() => decreaseQty(item, item.quantity)}
-                        >
-                          -
-                        </button>
-                        <input
-                          type="number"
-                          className="w-12 text-center border border-gray-300 mx-2 rounded-md"
-                          value={item?.quantity}
-                          readOnly
-                        />
-                        <button
-                          className="px-3 py-1 bg-blue-500 text-white rounded-md"
-                          onClick={() => increaseQty(item, item.quantity)}
-                        >
-                          +
-                        </button>
-                      </div>
-
-                      <button
-                        className="text-red-600 hover:text-red-800 mx-4 text-2xl"
-                        onClick={() => removeCartItemHandler(item?.product)}
+                    <div class="flex items-center justify-center gap-2">
+                      <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                        {" "}
+                        or{" "}
+                      </span>
+                      <Link
+                        to={"/"}
+                        class="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
                       >
-                        <RiDeleteBin6Fill />
-                      </button>
+                        Continue Shopping
+                        <svg
+                          class="h-5 w-5"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M19 12H5m14 0-4 4m4-4-4-4"
+                          />
+                        </svg>
+                      </Link>
                     </div>
                   </div>
-                ))}
+                </div>
               </div>
-
-              {/* Order Summary */}
-              <div className="w-full lg:w-1/3 mt-6 lg:mt-0 p-6 bg-white shadow-md rounded-md">
-                <h4 className="text-xl font-semibold mb-4">Order Summary</h4>
-                <hr className="border-gray-300 mb-4" />
-                <p className="text-lg">
-                  Units:{" "}
-                  <span className="font-semibold">
-                    {cartItems?.reduce((acc, item) => acc + item?.quantity, 0)}{" "}
-                    (Units)
-                  </span>
-                </p>
-                <p className="text-lg mt-2">
-                  Est. total:{" "}
-                  <span className="font-semibold text-green-600">
-                    $
-                    {cartItems
-                      ?.reduce(
-                        (acc, item) => acc + item?.quantity * item.price,
-                        0
-                      )
-                      .toFixed(2)}
-                  </span>
-                </p>
-                <hr className="border-gray-300 my-4" />
-                <button className="w-full bg-blue-500 text-white py-2 rounded-md font-semibold hover:bg-blue-600">
-                  Check out
-                </button>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
+            </>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
