33323c0fb27d533e924d792ef9653ded5e13d2a2
Tae <phenphak.forlearn@gmail.com>
2025-03-22 16:37:53 +0700

    refactor: simplify payment processing logic

--- frontend/src/redux/api/orderApi.js
+++ frontend/src/redux/api/orderApi.js
@@ -5,15 +5,21 @@ export const orderApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
   endpoints: (builder) => ({
     createNewOrder: builder.mutation({
-      query(body) {
-        return {
-          url: "/orders/new",
-          method: "POST",
-          body,
-        };
-      },
+      query: (body) => ({
+        url: "/orders/new",
+        method: "POST",
+        body,
+      }),
+    }),
+    stripeCheckoutSession: builder.mutation({
+      query: (body) => ({
+        url: "/payment/checkout_session",
+        method: "POST",
+        body,
+      }),
     }),
   }),
 });
 
-export const { useCreateNewOrderMutation } = orderApi;
+export const { useCreateNewOrderMutation, useStripeCheckoutSessionMutation } =
+  orderApi;
