9ef55a8bae292adc7f6324bf5e5da40db4798eee
Tae <phenphak.forlearn@gmail.com>
2025-04-21 17:53:29 +0700

    feat: add PRODUCT_SIZES constant

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,9 +1,11 @@
 export const PRODUCT_CATEGORIES = [
-    "Meals",
-    "Snacks & Desserts",
-    "Drinks",
-    "Breakfast",
-    "Kids Healthy Food",
-    "Healthy Oils & Spreads",
-    "Organic",
-  ];
+  "Meals",
+  "Snacks & Desserts",
+  "Drinks",
+  "Breakfast",
+  "Kids Healthy Food",
+  "Healthy Oils & Spreads",
+  "Organic",
+];
+
+export const PRODUCT_SIZES = ["small", "medium", "large"];
