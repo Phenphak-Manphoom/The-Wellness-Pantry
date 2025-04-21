bfaded8edf5960a0e17468219f5e64a2db448d3f
Tae <phenphak.forlearn@gmail.com>
2025-04-21 17:41:23 +0700

    refactor: simplify and improve reusability of SideMenu component

--- frontend/src/components/layout/SideMenu.jsx
+++ frontend/src/components/layout/SideMenu.jsx
@@ -1,36 +1,10 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { User, Lock, Image, Pencil } from "lucide-react"; // Import ไอคอน
+
 import classNames from "classnames"; // สำหรับจัดการคลาส
 
-const SideMenu = () => {
+const SideMenu = ({ menuItems }) => {
   const location = useLocation();
-
-  const menuItems = useMemo(
-    () => [
-      {
-        name: "Profile",
-        url: "/me/profile",
-        icon: <User size={20} className="mr-2" />,
-      },
-      {
-        name: "Update Profile",
-        url: "/me/update_profile",
-        icon: <Pencil size={20} className="mr-2" />,
-      },
-      {
-        name: "Upload Avatar",
-        url: "/me/upload_avatar",
-        icon: <Image size={20} className="mr-2" />,
-      },
-      {
-        name: "Update Password",
-        url: "/me/update_password",
-        icon: <Lock size={20} className="mr-2" />,
-      },
-    ],
-    []
-  );
 
   return (
     <div className="m-auto space-y-2">
@@ -49,7 +23,13 @@ const SideMenu = () => {
             }
           )}
         >
-          {menuItem.icon} {menuItem.name}
+          {menuItem.icon &&
+            (typeof menuItem.icon === "string" ? (
+              <i className={`${menuItem.icon} mr-2`} />
+            ) : (
+              <span className="mr-2">{menuItem.icon}</span>
+            ))}
+          {menuItem.name}
         </Link>
       ))}
     </div>
